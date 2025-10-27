81f98d9fb232752a6a5954d2fa5ac208e0c027a1
Shin HyunKyu <hkshin0706@gmail.com>
2025-10-27 20:06:50 +0900

    content script

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,3 +4,15 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
 });
+
+export const contentScripts = [
+  {
+    matches: ["https://hansung.ac.kr/cncschool/7309/*"], // 스크립트를 실행할 URL 패턴
+    js: ["content-scripts/modify-goods-ui.ts"]
+  },
+  {
+    matches: ["https://www.hansung.ac.kr/onestop/8952/*"], // 스크립트를 실행할 URL 패턴
+    js: ["content-scripts/modify-space-ui.ts"]
+  }
+];
+
