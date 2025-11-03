e1fda463e7476bd9c970dc9164ba02f1ecc80e08
석 <dudtjr309@gmail.com>
2025-11-03 18:27:38 +0900

    feat: update three links to HSU ...

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -29,8 +29,20 @@ function App() {
                   imgSrc="https://i.namu.wiki/i/cS0GHv6Ougi3uEDW-HkDBfgbtiVpLFyJBn5fO-XN7I47zVx-81-7WSoJtMD9op-gkGI6POqxKTkqe1g5n6e-NQ.webp"
                   alt="HSU e-class"
               />
+                <LinkArea
+                    url="https://www.hansung.ac.kr/onestop/8952/subview.do"
+                    text="상상베이스 세미나실 예약"
+                    imgSrc="https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/ad5c5ae9-176f-57a1-97fd-20afcb32a355/8f037b8a-487e-5bc4-8aef-95264a468c03.jpg"
+                    alt="HSU 상상베이스 세미나실 예약"
+                />
+                <LinkArea
+                    url="https://hansung.ac.kr/cncschool/7309/subview.do"
+                    text="기자재 대여"
+                    imgSrc="https://hansung.ac.kr/sites/cncschool/images/%EC%9E%A5%EB%B9%84%20%EC%9D%B4%EB%AF%B8%EC%A7%80/K1%20MAX.png"
+                    alt="HSU 기자재 대여"
+                />
               <button onClick={() => {
-                  // 새로운 것 구현
+                  // 새로운 것 추가 or 새로운 페이지 추가
               }}> + </button>
         </div>
       </div>
